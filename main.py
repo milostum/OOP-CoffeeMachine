bbb100c9e03a8487bc8849c61962d934f36cdad1
Milos Stojanovic <50637826+milostum@users.noreply.github.com>
2020-12-30 23:30:25 +0100

    Update main.py
    Final version.

--- main.py
+++ main.py
@@ -22,3 +22,5 @@ while is_on:
             if coffee_maker.is_resource_sufficient(drink) and money_machine.make_payment(drink.cost):
                 coffee_maker.make_coffee(drink)
                 money_machine.money_received = 0
+
+                
